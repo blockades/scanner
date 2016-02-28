fe559c44d5f0aa43ef8f5d07f1c679675c70a52c
dan-mi-sun <dan.mi.sun@gmail.com>
2016-02-28 12:45:30 +0100

    stuck at this point getrawtransaction producing a Right - not yet sure what to do with this

--- src/main/scala/org/dyne/danielsan/superchain/data/bitcoinrpc/JSONRPC.scala
+++ src/main/scala/org/dyne/danielsan/superchain/data/bitcoinrpc/JSONRPC.scala
@@ -112,5 +112,5 @@ object JSONRPCMain extends App{
 
   val resp = BitcoinRPC.getrawtransaction("0e3e2357e806b6cdb1f70b54c3a3a17b6714ee1f0e68bebb44a74b1efd512098", true)
 
-  println(resp)
+  println(resp.getClass)
 }
