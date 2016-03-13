e0f86e063f96c5a28f1c685afc21d09c82855706
dan-mi-sun <dan.mi.sun@gmail.com>
2016-03-13 11:38:00 +0000

    Enabling Mockito in TestStack

--- src/test/scala/org/dyne/danielsan/superchain/testsupport/TestStack.scala
+++ src/test/scala/org/dyne/danielsan/superchain/testsupport/TestStack.scala
@@ -6,4 +6,4 @@ import org.scalatest.{FunSpecLike, Matchers}
 /**
   * Created by dan_mi_sun on 10/03/2016.
   */
-trait TestStack extends Matchers with FunSpecLike // with MockitoSugar
+trait TestStack extends Matchers with FunSpecLike with MockitoSugar
