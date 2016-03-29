395d80190427d95ffa960c66c93e330e48d96639
dan-mi-sun <dan.mi.sun@gmail.com>
2016-03-29 15:18:46 -0700

    work in progress with Transaction Model

--- src/main/scala/org/dyne/danielsan/superchain/data/models/Transaction.scala
+++ src/main/scala/org/dyne/danielsan/superchain/data/models/Transaction.scala
@@ -1,41 +1,16 @@
 package org.dyne.danielsan.superchain.data.models
 
+import com.websudos.phantom.CassandraTable
+import com.websudos.phantom.dsl._
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
+
+import scala.concurrent.Future
+
+
 /**
   * Created by dan_mi_sun on 27/02/2016.
   */
-
-/*
-// The following just me trying to figure out what fields constitute a Transaction
-// This is just a 'spreadout' version of the output gathered from bitcoin-cli getrawtransaction
-
-{"blockhash":"00000000839a8e6886ab5951d76f411475428afc90947ee320161bbf18eb6048",
-  "blocktime":1231469665,
-  "hex":"01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff0704ffff001d0104ffffffff0100f
-  2052a0100000043410496b538e853519c726a2c91e61ec11600ae1390813a627c66fb8be7947be63c52da7589379515d4e0a604f8141781e622947
-    21166bf621e73a82cbf2342c858eeac00000000",
-  "confirmations":269439,
-  "txid":"0e3e2357e806b6cdb1f70b54c3a3a17b6714ee1f0e68bebb44a74b1efd512098",
-  "vout":[
-  {"value":50.00000000,
-  "n":0,
-  "scriptPubKey":
-  {"hex":"410496b538e853519c726a2c91e61ec11600ae1390813a627c66fb8be7947be63c52da7589379515d4e0a604f8141781e62294721166
-  bf621e73a82cbf2342c858eeac",
-  "asm":"0496b538e853519c726a2c91e61ec11600ae1390813a627c66fb8be7947be63c52da7589379515d4e0a604f8141781e62294721166bf6
-  21e73a82cbf2342c858ee OP_CHECKSIG",
-  "type":"pubkey",
-  "reqSigs":1,
-  "addresses":["12c6DSiU4Rq3P4ZxziKxzrL5LmMBrzjrJX"]}}],
-  "version":1,
-  "vin":[{"coinbase":"04ffff001d0104","sequence":4294967295}],
-  "time":1231469665S
-  "locktime":0
-  }
-  */
-
-// Put up a question here: http://stackoverflow.com/questions/35747753/complex-encoding-of-multiple-nested-classes-using-scala-argonaut
-// Easier to read code here: https://gist.github.com/mbbx6spp/df771b3aaaea90d20dd0
-
 
 case class Transaction(blockhash: String,
                        blocktime: Long,
@@ -60,4 +35,98 @@ case class ScriptPubKey(hex: String,
 
 case class Vin(coinbase: String,
                sequence: Int)
+
+/*
+One of the issues that I am running into below is that:
+"Cassandra collections do not allow custom data types. Storing JSON as a string is possible, but it's still a text column as far as Cassandra is concerned. The type in the below example is always a default C* type."
+
+https://github.com/outworkers/phantom/wiki/Collection-columns
+
+ */
+
+sealed class TransactionColumnFamily extends CassandraTable[TransactionColumnFamily, Transaction] {
+
+  implicit val formats = DefaultFormats
+
+  override def fromRow(row: Row): Transaction = {
+    Transaction(
+      blockhash(row),
+      blocktime(row),
+      hex(row),
+      confirmations(row),
+      txid(row),
+      vout(row),
+      version(row),
+      vin(row),
+      time(row),
+      locktime(row)
+    )
+  }
+
+  object blockhash extends StringColumn(this) with PartitionKey[String]
+
+  object blocktime extends LongColumn(this) with ClusteringOrder[Long] with Descending
+
+  object hex extends StringColumn(this) with ClusteringOrder[String] with Descending
+
+  object confirmations extends IntColumn(this) with ClusteringOrder[Int] with Descending
+
+  object txid extends StringColumn(this) with ClusteringOrder[String] with Descending
+
+  object vout extends JsonListColumn[TransactionColumnFamily, Transaction, JValue](this) {
+    override def fromJson(obj: String): JValue = {
+      parse(obj).extract[JValue]
+    }
+
+    override def toJson(obj: JValue): String = {
+      compact(render(obj))
+    }
+  }
+
+  object version extends IntColumn(this) with ClusteringOrder[Int] with Descending
+
+  object vin extends JsonListColumn[TransactionColumnFamily, Transaction, JValue](this) {
+    override def fromJson(obj: String): JValue = {
+      parse(obj).extract[JValue]
+    }
+
+    override def toJson(obj: JValue): String = {
+      compact(render(obj))
+    }
+  }
+
+  object time extends IntColumn(this) with ClusteringOrder[Int] with Descending
+
+  object locktime extends IntColumn(this) with ClusteringOrder[Int] with Descending
+
+}
+
+abstract class TransactionTable extends TransactionColumnFamily with RootConnector {
+
+  override val tableName = "transactions"
+
+  def insertNew(tx: Transaction): Future[ResultSet] = insertNewTransaction(tx).future()
+
+  def insertNewTransaction(tx: Transaction) = {
+    insert
+      .value(_.blockhash, tx.blockhash)
+      .value(_.blocktime, tx.blocktime)
+      .value(_.hex, tx.hex)
+      .value(_.confirmations, tx.confirmations)
+      .value(_.txid, tx.txid)
+      .value(_.vout, tx.vout)
+      .value(_.version, tx.version)
+      .value(_.vin, tx.vin)
+      .value(_.time, tx.time)
+      .value(_.locktime, tx.locktime)
+  }
+
+
+}
+
+//object TransactionColumnFamily extends TransactionColumnFamily with RootConnector {
+////  figure out how to list -> this can come from other example working from
+////    figure out how to insert https :// github.com / outworkers / phantom / wiki / Collection - columns
+//
+//}
 
