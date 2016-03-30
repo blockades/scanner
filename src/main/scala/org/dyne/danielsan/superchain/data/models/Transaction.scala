0876e8a32e3cf49029fa7eef73d12840e57c4dea
dan-mi-sun <dan.mi.sun@gmail.com>
2016-03-30 00:48:28 -0700

    wip: transaction phantom-dsl toJson serializer in progress

--- src/main/scala/org/dyne/danielsan/superchain/data/models/Transaction.scala
+++ src/main/scala/org/dyne/danielsan/superchain/data/models/Transaction.scala
@@ -17,14 +17,10 @@ case class Transaction(txid: String,
                        locktime: Int,
                        vin: List[Vin],
                        vout: List[Vout]
-                      /*
-                       blockhash: String,
-                       blocktime: Long,
-                       hex: String,
-                       confirmations: Int,
-                       time: Int
-                       */
                       )
+
+case class Vin(coinbase: String,
+               sequence: Int)
 
 case class Vout(value: Float,
                 n: Int,
@@ -36,100 +32,78 @@ case class ScriptPubKey(hex: String,
                         reqSigs: Int,
                         addresses: List[String])
 
-case class Vin(coinbase: String,
-               sequence: Int)
 
 /*
 One of the issues that I am running into below is that:
 "Cassandra collections do not allow custom data types. Storing JSON as a string is possible, but it's still a text column as far as Cassandra is concerned. The type in the below example is always a default C* type."
 
 https://github.com/outworkers/phantom/wiki/Collection-columns
+*/
 
- */
+sealed class TransactionColumnFamily extends CassandraTable[TransactionColumnFamily, Transaction] {
 
-//sealed class TransactionColumnFamily extends CassandraTable[TransactionColumnFamily, Transaction] {
-//
-//  implicit val formats = DefaultFormats
-//
-//  override def fromRow(row: Row): Transaction = {
-//    Transaction(
-//      blockhash(row),
-//      blocktime(row),
-//      hex(row),
-//      confirmations(row),
-//      txid(row),
-//      vout(row),
-//      version(row),
-//      vin(row),
-//      time(row),
-//      locktime(row)
-//    )
-//  }
-//
-//  object blockhash extends StringColumn(this) with PartitionKey[String]
-//
-//  object blocktime extends LongColumn(this) with ClusteringOrder[Long] with Descending
-//
-//  object hex extends StringColumn(this) with ClusteringOrder[String] with Descending
-//
-//  object confirmations extends IntColumn(this) with ClusteringOrder[Int] with Descending
-//
-//  object txid extends StringColumn(this) with ClusteringOrder[String] with Descending
-//
-//  object vout extends JsonListColumn[TransactionColumnFamily, Transaction, JValue](this) {
-//    override def fromJson(obj: String): JValue = {
-//      parse(obj).extract[JValue]
-//    }
-//
-//    override def toJson(obj: JValue): String = {
-//      compact(render(obj))
-//    }
-//  }
-//
-//  object version extends IntColumn(this) with ClusteringOrder[Int] with Descending
-//
-//  object vin extends JsonListColumn[TransactionColumnFamily, Transaction, JValue](this) {
-//    override def fromJson(obj: String): JValue = {
-//      parse(obj).extract[JValue]
-//    }
-//
-//    override def toJson(obj: JValue): String = {
-//      compact(render(obj))
-//    }
-//  }
-//
-//  object time extends IntColumn(this) with ClusteringOrder[Int] with Descending
-//
-//  object locktime extends IntColumn(this) with ClusteringOrder[Int] with Descending
-//
-//}
-//
-//abstract class TransactionTable extends TransactionColumnFamily with RootConnector {
-//
-//  override val tableName = "transactions"
-//
-//  def insertNew(tx: Transaction): Future[ResultSet] = insertNewTransaction(tx).future()
-//
-//  def insertNewTransaction(tx: Transaction) = {
-//    insert
-//      .value(_.blockhash, tx.blockhash)
-//      .value(_.blocktime, tx.blocktime)
-//      .value(_.hex, tx.hex)
-//      .value(_.confirmations, tx.confirmations)
-//      .value(_.txid, tx.txid)
-//      .value(_.vout, tx.vout)
-//      .value(_.version, tx.version)
-//      .value(_.vin, tx.vin)
-//      .value(_.time, tx.time)
-//      .value(_.locktime, tx.locktime)
-//  }
-//
-//
-//}
+  implicit val formats = DefaultFormats
 
-//object TransactionColumnFamily extends TransactionColumnFamily with RootConnector {
-////  figure out how to list -> this can come from other example working from
-////    figure out how to insert https :// github.com / outworkers / phantom / wiki / Collection - columns
-//
-//}
+  override def fromRow(row: Row): Transaction = {
+    Transaction(
+      txid(row),
+      version(row),
+      locktime(row),
+      vin(row),
+      vout(row)
+    )
+  }
+
+  object txid extends StringColumn(this) with PartitionKey[String]
+
+  object version extends IntColumn(this) with ClusteringOrder[Int] with Descending
+
+  object locktime extends IntColumn(this) with ClusteringOrder[Int] with Descending
+
+  object vout extends JsonListColumn[TransactionColumnFamily, Transaction, Vout](this) {
+    override def fromJson(obj: String): Vout = {
+      parse(obj).extract[Vout]
+    }
+
+    //this is where we have gotten to again
+    // need to figure out the serializer
+    override def toJson(obj: Vout): String = {
+      compact(render(obj))
+    }
+  }
+
+  object vin extends JsonListColumn[TransactionColumnFamily, Transaction, Vin](this) {
+    override def fromJson(obj: String): Vin = {
+      parse(obj).extract[Vin]
+    }
+
+    override def toJson(obj: Vin): String = {
+      compact(render(obj))
+    }
+  }
+
+}
+
+abstract class TransactionTable extends TransactionColumnFamily with RootConnector {
+
+  override val tableName = "transactions"
+
+  def insertNew(tx: Transaction): Future[ResultSet] = insertNewTransaction(tx).future()
+
+  def insertNewTransaction(tx: Transaction) = {
+    insert
+      .value(_.txid, tx.txid)
+      .value(_.vout, tx.vout)
+      .value(_.version, tx.version)
+      .value(_.vin, tx.vin)
+      .value(_.locktime, tx.locktime)
+  }
+
+}
+
+object TransactionColumnFamily extends TransactionColumnFamily with RootConnector {
+//  figure out how to list -> this can come from other example working from
+//    figure out how to insert https :// github.com / outworkers / phantom / wiki / Collection - columns
+
+}
 
