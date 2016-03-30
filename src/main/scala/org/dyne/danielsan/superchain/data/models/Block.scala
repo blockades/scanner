e523332894e9ee104708eec4c2c407623be53e7f
dan-mi-sun <dan.mi.sun@gmail.com>
2016-03-30 00:14:22 -0700

    tidying block model

--- src/main/scala/org/dyne/danielsan/superchain/data/models/Block.scala
+++ src/main/scala/org/dyne/danielsan/superchain/data/models/Block.scala
@@ -11,30 +11,6 @@ import scala.concurrent.Future
   * Created by dan_mi_sun on 27/02/2016.
   */
 
-/*
-$ bitcoin-cli getblock 00000000839a8e6886ab5951d76f411475428afc90947ee320161bbf18eb6048
-
-
-{
-"hash" : "00000000839a8e6886ab5951d76f411475428afc90947ee320161bbf18eb6048",
-"confirmations" : 299191,
-"size" : 215,
-"height" : 1,
-"version" : 1,
-"merkleroot" : "0e3e2357e806b6cdb1f70b54c3a3a17b6714ee1f0e68bebb44a74b1efd512098",
-"tx" : [
-"0e3e2357e806b6cdb1f70b54c3a3a17b6714ee1f0e68bebb44a74b1efd512098"
-],
-"time" : 1231469665,
-"nonce" : 2573394689,
-"bits" : "1d00ffff",
-"difficulty" : 1.00000000,
-"chainwork" : "0000000000000000000000000000000000000000000000000000000200020002",
-"previousblockhash" : "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f",
-"nextblockhash" : "000000006a625f06636b8bb6ac7b960a8d03705d1ace08b1a19da3fdcc99ddbd"
-}
-*/
-
 case class Block(hash: String,
                  confirmations: Int,
                  size: Int,
@@ -133,26 +109,6 @@ sealed class BlockColumnFamily extends CassandraTable[BlockColumnFamily, Block] 
     def destroy(hash: String): Future[ResultSet] = {
       delete.where(_.hash eqs hash).future()
     }
-
-//    def updateBlockEntry(block: Block) = {
-//      update.where(_.hash eqs block.hash)
-//        .modify(_.confirmations setTo block.confirmations)
-//        .and(_.size setTo block.size)
-//        .and(_.height setTo block.height)
-//        .and(_.version setTo block.version)
-//        .and(_.merkleroot setTo block.merkleroot)
-//        .and(_.tx setTo block.tx)
-//        .and(_.time setTo block.time)
-//        .and(_.nonce setTo block.nonce)
-//        .and(_.bits setTo block.bits)
-//        .and(_.difficulty setTo block.difficulty)
-//        .and(_.chainwork setTo block.chainwork)
-//        .and(_.previousblockhash setTo block.previousblockhash)
-//        .and(_.nextblockhash setTo block.nextblockhash)
-//        .consistencyLevel_=(ConsistencyLevel.ALL)
-//        .future()
-//      getByHash(block.hash)
-//    }
 
     def getByHash(hash: String): Future[Option[Block]] = {
       select.where(_.hash eqs hash).one()
