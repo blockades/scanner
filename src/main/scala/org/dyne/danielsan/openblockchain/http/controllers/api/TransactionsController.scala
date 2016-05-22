e8186257a8c829b3d67df14c0b1e2bc67593401d
dan-mi-sun <dan.mi.sun@gmail.com>
2016-05-22 22:36:46 +1000

    adding swagger docs for TransactionsController

--- src/main/scala/org/dyne/danielsan/openblockchain/http/controllers/api/TransactionsController.scala
+++ src/main/scala/org/dyne/danielsan/openblockchain/http/controllers/api/TransactionsController.scala
@@ -1,8 +1,10 @@
 package org.dyne.danielsan.openblockchain.http.controllers.api
 
 import org.dyne.danielsan.openblockchain.data.database.ChainDatabase
+import org.dyne.danielsan.openblockchain.http.controllers.api.swaggerdocs.TransactionsControllerDocs
 import org.json4s.{DefaultFormats, Formats}
 import org.scalatra.json.JacksonJsonSupport
+import org.scalatra.swagger.Swagger
 import org.scalatra.{FutureSupport, ScalatraServlet}
 
 import scala.concurrent.Await
@@ -12,7 +14,7 @@ import scala.concurrent.duration._
 /**
   * Created by dan_mi_sun on 22/05/2016.
   */
-class TransactionsController extends ScalatraServlet with FutureSupport with JacksonJsonSupport {
+class TransactionsController (implicit val swagger: Swagger) extends ScalatraServlet with FutureSupport with JacksonJsonSupport with TransactionsControllerDocs {
 
   protected implicit lazy val jsonFormats: Formats = DefaultFormats
   val executor = scala.concurrent.ExecutionContext.global
@@ -21,11 +23,11 @@ class TransactionsController extends ScalatraServlet with FutureSupport with Jac
     contentType = formats("json")
   }
 
-  get("/") {
+  get("/", operation(getTransactions)) {
     ChainDatabase.listAllTransactions
   }
 
-  get("/:id") {
+  get("/:id", operation(getTransaction)) {
     val id = params("id")
     Await.result(ChainDatabase.getTransactionByTxid(id), 3.seconds) match {
       case Some(block) => block
