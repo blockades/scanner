07ca43e4b4e0e7b54cb014ddbad42d63c8d3814f
dan-mi-sun <dan.mi.sun@gmail.com>
2016-05-28 07:20:22 +1000

    updating swagger documentation with mock data links to github

--- src/main/scala/org/dyne/danielsan/openblockchain/http/controllers/api/swaggerdocs/TransactionsControllerDocs.scala
+++ src/main/scala/org/dyne/danielsan/openblockchain/http/controllers/api/swaggerdocs/TransactionsControllerDocs.scala
@@ -12,12 +12,12 @@ trait TransactionsControllerDocs extends SwaggerSupport {
   val getTransactions =
     (apiOperation[List[Transaction]]("getTransactions")
       summary "Shows all Transactions"
-      notes "Currently shows all transactions, with no possibility of filtering them. TODO: add filtering functionality.")
+      notes "Currently shows all transactions, with no possibility of filtering them. TODO: add filtering functionality. Mock data can be found here: https://github.com/dan-mi-sun/openblockchain/blob/feature/webapp/src/main/resources/mocks/transactions.csv ")
 
   val getTransaction =
     (apiOperation[Transaction]("getTransaction")
       summary "Shows a transaction"
-      notes "Retrieves a transaction by its txid"
+      notes "Retrieves a transaction by its txid. Mock data can be found here: https://github.com/dan-mi-sun/openblockchain/blob/feature/webapp/src/main/resources/mocks/transactions.csv"
       parameters
       pathParam[String]("id").description("ID of the transaction that needs to be fetched").required
       responseMessage StringResponseMessage(200, "Transaction retrieved"))
