b2a760552a719153ccdb7bfdb57ea96752b99fd4
Dan Hassan <daniel.san@dyne.org>
2016-08-02 08:18:01 +1000

    updating swagger documentation for blocks & transactions

--- src/main/scala/org/dyne/danielsan/openblockchain/http/controllers/api/swaggerdocs/TransactionsControllerDocs.scala
+++ src/main/scala/org/dyne/danielsan/openblockchain/http/controllers/api/swaggerdocs/TransactionsControllerDocs.scala
@@ -12,12 +12,12 @@ trait TransactionsControllerDocs extends SwaggerSupport {
   val getTransactions =
     (apiOperation[List[Transaction]]("getTransactions")
       summary "Shows all Transactions"
-      notes "Currently shows all transactions, with no possibility of filtering them. TODO: add filtering functionality. Mock data can be found here: https://github.com/dan-mi-sun/openblockchain/blob/feature/webapp/src/main/resources/mocks/transactions.json ")
+      notes "Currently shows all transactions, with no possibility of filtering them. TODO: add filtering functionality.")
 
   val getTransaction =
     (apiOperation[Transaction]("getTransaction")
       summary "Shows a transaction"
-      notes "Retrieves a transaction by its txid. Mock data can be found here: https://github.com/dan-mi-sun/openblockchain/blob/feature/webapp/src/main/resources/mocks/transactions.json"
+      notes "Retrieves a transaction by its txid."
       parameters
       pathParam[String]("id").description("ID of the transaction that needs to be fetched").required
       responseMessage StringResponseMessage(200, "Transaction retrieved"))
