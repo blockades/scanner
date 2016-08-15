6fe0a9c9960402034d92b5b82fe50dd009b358f3
Dan Hassan <daniel.san@dyne.org>
2016-08-15 23:28:52 +1000

    moving data assets to data repo

--- src/main/scala/org/dyne/danielsan/openblockchain/data/entity/Vout.scala
+++
@@ -1,9 +0,0 @@
-package org.dyne.danielsan.openblockchain.data.entity
-
-/**
-  * Created by dan_mi_sun on 12/05/2016.
-  */
-case class Vout(value: Float,
-                n: Int,
-                scriptPubKey: ScriptPubKey
-               )
