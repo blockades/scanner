8cf250a65b0c717bf4566571407203a6de57869c
Dan Hassan <daniel.san@dyne.org>
2016-08-15 22:32:32 +1000

    BitcoinClientTest passing

--- src/test/scala/org/dyne/danielsan/openblockchain/client/BitcoinClientTest.scala
+++ src/test/scala/org/dyne/danielsan/openblockchain/client/BitcoinClientTest.scala
@@ -1,43 +1,33 @@
 package org.dyne.danielsan.openblockchain.client
 
-import org.dyne.danielsan.openblockchain.testsupport.TestStack
-
-import org.json4s._
+import org.dyne.danielsan.openblockchain.UnitTest
 import org.json4s.jackson.JsonMethods._
-
+import org.json4s.{DefaultFormats, _}
 
 /**
   * Created by dan_mi_sun on 10/03/2016.
   */
 class BitcoinClientTest extends TestStack {
+
   implicit val formats = DefaultFormats
 
-
-  describe("Our BitcoinClient") {
+  describe("A BitcoinClient") {
 
     val client = new BitcoinClient
 
-    describe("Grabbing a transaction blockhash") {
-      describe("given an integer") {
-        val id = 1
-        val correctResult = "00000000839a8e6886ab5951d76f411475428afc90947ee320161bbf18eb6048"
-
-        it("should return the hash of that block") {
-          client.getBlockHashForId(id) shouldEqual correctResult
-        }
-      }
+    it("should return the hash of a block by id") {
+      val id = 1
+      val expectedHash = "00000000839a8e6886ab5951d76f411475428afc90947ee320161bbf18eb6048"
+      client.getBlockHashForId(id) shouldEqual expectedHash
     }
 
-
-    //Have commented out this test as it should be correct, but the confirmations are always dynamic and changing depending
-    //on the latest head - so need to figure out how to mock this as the value will always change
-
-    describe("grabbing a block") {
-      val jsonString =
+    it("should return a block by hash") {
+      val hash = "00000000839a8e6886ab5951d76f411475428afc90947ee320161bbf18eb6048"
+      val expectedJson =
         """
           |{
           |    "hash" : "00000000839a8e6886ab5951d76f411475428afc90947ee320161bbf18eb6048",
-          |    "confirmations" : 334142,
+          |    "confirmations" : 0,
           |    "size" : 215,
           |    "height" : 1,
           |    "version" : 1,
@@ -53,20 +43,20 @@ class BitcoinClientTest extends TestStack {
           |    "previousblockhash" : "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f",
           |    "nextblockhash" : "000000006a625f06636b8bb6ac7b960a8d03705d1ace08b1a19da3fdcc99ddbd"
           |}
-        """.stripMargin
+          | """.stripMargin
 
-      //val block = parse(jsonString).extract[Block]
+      val blockString = client.getBlockStringForHash(hash)
+      val mergeResetJson = parse("""{ "result": { "confirmations": 0 } }""")
+      val blockJson = parse(blockString).merge(mergeResetJson) // reset confirmations to 0
+      val block = blockJson \ "result"
+      val expectedBlock = parse(expectedJson)
 
-      it("should give back the block") {
-        client.getBlockForHash("00000000839a8e6886ab5951d76f411475428afc90947ee320161bbf18eb6048") shouldEqual jsonString
-      }
+      block shouldEqual expectedBlock
     }
 
-
-    describe("creating a chain of blocks") {
-      //the difficulty here is that we don't want to set the whole thing in motion
-      // how best to mock out the response to one we expect?
-    }
+    it("should ... chain of blocks")(pending)
+    // the difficulty here is that we don't want to set the whole thing in motion
+    // how best to mock out the response to one we expect?
 
   }
 
