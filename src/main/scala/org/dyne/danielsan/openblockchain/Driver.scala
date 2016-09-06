514656fec058e76942884457e9230b736677bd9b
Dan Hassan <daniel.san@dyne.org>
2016-09-06 22:42:13 +1000

    adding is_op_return to BlockTransactions

--- src/main/scala/org/dyne/danielsan/openblockchain/Driver.scala
+++ src/main/scala/org/dyne/danielsan/openblockchain/Driver.scala
@@ -3,7 +3,6 @@ package org.dyne.danielsan.openblockchain
 import org.dyne.danielsan.openblockchain.client.BitcoinClient
 import org.dyne.danielsan.openblockchain.data.database.ChainDatabase
 import org.dyne.danielsan.openblockchain.data.model.BlockTransactions
-
 import org.json4s.DefaultFormats
 
 import scala.concurrent.Await
@@ -29,26 +28,32 @@ object Driver {
     for (id <- 1 to 5) {
 
       val block = client.getBlockForId(id)
-      println("Block: " + block)
+      println(block)
       val blockInsertOp = ChainDatabase.insertBlock(block)
       Await.result(blockInsertOp, 10.seconds)
 
       val txs = client.getRawTransaction(id)
-      println("Transaction: " + txs)
+      println(txs)
       txs.foreach { tx =>
-        val btx = BlockTransaction(block.hash, tx.txid)
-        println("BlockTransaction:" + btx)
-        val btInsertOp = ChainDatabase.insertBlockTransaction(btx)
-        Await.result(btInsertOp , 10.seconds)
+        val txInsertOp = ChainDatabase.insertTransaction(tx)
+        Await.result(txInsertOp, 10.seconds)
+
+        tx.vout.foreach { vout =>
+          val isOpReturnTx = vout.scriptPubKey.asm.contains("OP_CHECKSIG")
+          val btx = BlockTransactions(block.hash, tx.txid, vout.n, isOpReturnTx)
+          println(btx)
+          val btInsertOp = ChainDatabase.insertBlockTransaction(btx)
+          Await.result(btInsertOp, 10.seconds)
+        }
       }
 
       val btc = client.updateBlockTransactionCount(id)
-      println("BlockTansactionCount: " + btc)
+      println(btc)
       val updateBtcOp = ChainDatabase.saveOrUpdateBlockTransactionCount(btc)
       Await.result(updateBtcOp, 10.seconds)
 
       val op_return_count = client.updateBlockOpReturnTransactionCount(id)
-      println("BlockOpReturnTansactionCount: " + op_return_count)
+      println(op_return_count)
       val operationOpReturn = ChainDatabase.saveOrUpdateBlockOpReturnTransactionCount(op_return_count)
       Await.result(operationOpReturn, 10.seconds)
 
