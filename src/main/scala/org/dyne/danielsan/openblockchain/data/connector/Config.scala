110975f3e82eb0f089ac7ef2679e05a21d994c4b
Dan Hassan <daniel.san@dyne.org>
2016-09-11 00:16:25 +1000

    customising cassandra config

--- src/main/scala/org/dyne/danielsan/openblockchain/data/connector/Config.scala
+++ src/main/scala/org/dyne/danielsan/openblockchain/data/connector/Config.scala
@@ -1,5 +1,6 @@
 package org.dyne.danielsan.openblockchain.data.connector
 
+import com.datastax.driver.core.{Cluster, HostDistance, PoolingOptions, SocketOptions}
 import com.websudos.phantom.connectors._
 
 /**
@@ -7,19 +8,31 @@ import com.websudos.phantom.connectors._
   */
 object Config {
 
-  val hostsNum = sys.env.get("CASSANDRA_HOSTS_NUM").map(_.toInt).getOrElse(0)
-
-  val hosts = if (hostsNum > 0) {
-    1 to hostsNum map {
-      index => sys.env("CASSANDRA_HOST_" + index)
+  def clusterBuilder(builder: Cluster.Builder): Cluster.Builder = {
+    val hostsNum = sys.env.get("CASSANDRA_HOSTS_NUM").map(_.toInt).getOrElse(0)
+    val hosts = if (hostsNum > 0) {
+      1 to hostsNum map {
+        index => sys.env("CASSANDRA_HOST_" + index)
+      }
+    } else {
+      Seq(sys.env.getOrElse("CASSANDRA_HOST", "localhost"))
     }
-  } else {
-    Seq(sys.env.getOrElse("CASSANDRA_HOST", "localhost"))
-  }
-  val port = ContactPoint.DefaultPorts.live
 
-  val keySpace = KeySpace("openblockchain")
-  val contactPoint: KeySpaceBuilder = ContactPoints(hosts, port)
-  val keySpaceDefinition: KeySpaceDef = contactPoint.keySpace(keySpace.name)
+    val socketOptions = new SocketOptions()
+      .setConnectTimeoutMillis(120 * 1000)
+      .setReadTimeoutMillis(120 * 1000)
+
+    val poolingOptions = new PoolingOptions()
+      .setMaxRequestsPerConnection(HostDistance.LOCAL, 1024)
+      .setMaxRequestsPerConnection(HostDistance.REMOTE, 1024)
+      .setPoolTimeoutMillis(10 * 60 * 1000)
+
+    builder
+      .addContactPoints(hosts: _*)
+      .withSocketOptions(socketOptions)
+      .withPoolingOptions(poolingOptions)
+  }
+
+  val keySpaceDefinition: KeySpaceDef = new KeySpaceBuilder(clusterBuilder).keySpace("openblockchain")
 
 }
