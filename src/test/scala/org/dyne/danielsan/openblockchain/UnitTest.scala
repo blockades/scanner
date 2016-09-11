5b91a58bb6bfb8151773d5c57a52b8a7de49145d
Dan Hassan <daniel.san@dyne.org>
2016-09-11 21:04:54 +1000

    adding UnitTest

---
+++ src/test/scala/org/dyne/danielsan/openblockchain/UnitTest.scala
@@ -0,0 +1,9 @@
+package org.dyne.danielsan.openblockchain
+
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{FunSpec, Matchers}
+
+/**
+  * Created by aluxian on 07/08/16.
+  */
+abstract class UnitTest extends FunSpec with Matchers with MockitoSugar
