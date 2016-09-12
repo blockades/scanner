7fe979780f0d9298bac4ea9102e395e0a2f2e484
Dan Hassan <daniel.san@dyne.org>
2016-09-12 19:50:42 +1000

    refactoring UnitTest

--- src/test/scala/org/dyne/danielsan/openblockchain/UnitTest.scala
+++ src/test/scala/org/dyne/danielsan/openblockchain/UnitTest.scala
@@ -4,6 +4,6 @@ import org.scalatest.mock.MockitoSugar
 import org.scalatest.{FunSpec, Matchers}
 
 /**
-  * Created by aluxian on 07/08/16.
+  * Created by dan_mi_sun on 07/08/16.
   */
 abstract class UnitTest extends FunSpec with Matchers with MockitoSugar
